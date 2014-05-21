f695b971fb2415180b7091bbc0b78280bda5e7ff
azyobuzin <azyobuzin@users.sourceforge.jp>
2014-05-21 20:05:45 +0900

    Initial commit

---
+++ CoreTweetSupplement/CoreTweetSupplement.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CoreTweet.Core;
+
+namespace CoreTweet
+{
+    /// <summary>
+    /// Provides extensions for objects of CoreTweet.
+    /// </summary>
+    public static class CoreTweetSupplement
+    {
+        private static string CharFromInt(int code)
+        {
+            if (code <= char.MaxValue) return ((char)code).ToString();
+
+            code -= 0x10000;
+            return new string(new[]
+            {
+                (char)((code / 0x400) + 0xD800),
+                (char)((code % 0x400) + 0xDC00)
+            });
+        }
+
+        private static string HtmlDecode(string source)
+        {
+            if (!source.Contains("&")) return source;
+            var result = Regex.Replace(source, "&#([0-9]+);", match => CharFromInt(int.Parse(match.Groups[1].Value)));
+            result = Regex.Replace(result, "&#x([0-9a-f]+);",
+                match => CharFromInt(int.Parse(match.Groups[1].Value, NumberStyles.HexNumber)),
+                RegexOptions.IgnoreCase
+            );
+            return result.Replace("&nbsp;", " ")
+                .Replace("&lt;", "<")
+                .Replace("&gt;", ">")
+                .Replace("&amp;", "&")
+                .Replace("&quot;", "\"")
+                .Replace("&apos;", "'");
+        }
+
+        /// <summary>
+        /// Parse source field.
+        /// </summary>
+        /// <param name="html">The content of source field.</param>
+        /// <returns>A <see cref="CoreTweet.Source"/> instance.</returns>
+        public static Source ParseSource(string html)
+        {
+            if (!html.StartsWith("<"))
+                return new Source()
+                {
+                    Name = html
+                };
+
+            var match = Regex.Match(html, @"^<a href=""(.+)"" rel=""nofollow"">(.+)</a>$");
+            return match.Success
+                ? new Source()
+                {
+                    Name = HtmlDecode(match.Groups[2].Value),
+                    Href = new Uri(HtmlDecode(match.Groups[1].Value))
+                }
+                : new Source()
+                {
+                    Name = html
+                };
+        }
+
+        /// <summary>
+        /// Parse the html of <see cref="CoreTweet.Status.Source"/>.
+        /// </summary>
+        /// <param name="status">The <see cref="CoreTweet.Status"/> instance.</param>
+        /// <returns>A <see cref="CoreTweet.Source"/> instance.</returns>
+        public static Source ParseSource(this Status status)
+        {
+            return ParseSource(status.Source);
+        }
+
+        private static IEnumerable<string> EnumerateChar(string str)
+        {
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (char.IsSurrogatePair(str, i))
+                {
+                    yield return new string(new[] { str[i], str[++i] });
+                }
+                else
+                {
+                    yield return str[i].ToString();
+                }
+            }
+        }
+        
+        private class TextPart : ITextPart
+        {
+            public TextPartType Type { get; set; }
+            internal int Start { get; set; }
+            internal int End { get; set; }
+            public string RawText { get; set; }
+            public string Text { get; set; }
+            public CoreBase Entity { get; set; }
+        }
+
+        /// <summary>
+        /// Enumerates parts split into Tweet Entities.
+        /// </summary>
+        /// <param name="text">The text such as <see cref="CoreTweet.Status.Text"/>, <see cref="CoreTweet.DirectMessage.Text"/> and <see cref="CoreTweet.User.Description"/>.</param>
+        /// <param name="entities">The <see cref="CoreTweet.Entity"/> instance.</param>
+        /// <returns>An <see cref="T:System.Collections.Generic.IEnumerable{CoreTweet.ITextPart}"/> whose elements are parts of <paramref name="text"/>.</returns>
+        public static IEnumerable<ITextPart> EnumerateTextParts(string text, Entity entities)
+        {
+            if (entities == null)
+            {
+                yield return new TextPart()
+                {
+                    RawText = text,
+                    Text = HtmlDecode(text)
+                };
+                yield break;
+            }
+
+            var list = new LinkedList<TextPart>(
+                (entities.HashTags ?? Enumerable.Empty<HashTag>())
+                    .Select(e => new TextPart()
+                    {
+                        Type = TextPartType.Hashtag,
+                        Start = e.Indices[0],
+                        End = e.Indices[1],
+                        RawText = "#" + e.Text,
+                        Text = "#" + e.Text,
+                        Entity = e
+                    })
+                    .Concat(
+                        (entities.Media ?? Enumerable.Empty<Media>())
+                            .Select(e => new TextPart()
+                            {
+                                Type = TextPartType.Url,
+                                Start = e.Indices[0],
+                                End = e.Indices[1],
+                                RawText = e.Url.ToString(),
+                                Text = e.DisplayUrl,
+                                Entity = e
+                            })
+                    )
+                    .Concat(
+                        (entities.Urls ?? Enumerable.Empty<Url>())
+                            .Select(e => new TextPart()
+                            {
+                                Type = TextPartType.Url,
+                                Start = e.Indices[0],
+                                End = e.Indices[1],
+                                RawText = e.Uri.ToString(),
+                                Text = e.DisplayUrl,
+                                Entity = e
+                            })
+                    )
+                    .Concat(
+                        (entities.UserMentions ?? Enumerable.Empty<UserMention>())
+                            .Select(e => new TextPart()
+                            {
+                                Type = TextPartType.UserMention,
+                                Start = e.Indices[0],
+                                End = e.Indices[1],
+                                RawText = "@" + e.ScreenName,
+                                Text = "@" + e.ScreenName,
+                                Entity = e
+                            })
+                    )
+                    .OrderBy(part => part.Start)
+            );
+
+            if (list.Count == 0)
+            {
+                yield return new TextPart()
+                {
+                    RawText = text,
+                    Text = HtmlDecode(text)
+                };
+                yield break;
+            }
+
+            var current = list.First;
+            var chars = EnumerateChar(text).ToArray();
+
+            while (current != null)
+            {
+                var start = current.Previous != null ? current.Previous.Value.End : 0;
+                var count = current.Value.Start - start;
+                if (count > 0)
+                {
+                    var output = string.Concat(chars.Skip(start).Take(count));
+                    yield return new TextPart()
+                    {
+                        RawText = output,
+                        Text = HtmlDecode(output)
+                    };
+                }
+                
+                yield return current.Value;
+
+                if (current.Next == null) break;
+                current = current.Next;
+            }
+
+            var lastStart = current.Value.End;
+            if (lastStart < chars.Length)
+            {
+                var lastOutput = string.Concat(chars.Skip(lastStart));
+                yield return new TextPart()
+                {
+                    RawText = lastOutput,
+                    Text = HtmlDecode(lastOutput)
+                };
+            }
+        }
+
+        /// <summary>
+        /// Enumerates parts split into Tweet Entities.
+        /// </summary>
+        /// <param name="status">The <see cref="CoreTweet.Status"/> instance.</param>
+        /// <returns>An <see cref="T:System.Collections.Generic.IEnumerable{CoreTweet.ITextPart}"/> whose elements are parts of <paramref name="status"/>'s text.</returns>
+        public static IEnumerable<ITextPart> EnumerateTextParts(this Status status)
+        {
+            return EnumerateTextParts(status.Text, status.Entities);
+        }
+
+        /// <summary>
+        /// Enumerates parts split into Tweet Entities.
+        /// </summary>
+        /// <param name="dm">The <see cref="CoreTweet.DirectMessage"/> instance.</param>
+        /// <returns>An <see cref="T:System.Collections.Generic.IEnumerable{CoreTweet.ITextPart}"/> whose elements are parts of <paramref name="dm"/>'s text.</returns>
+        public static IEnumerable<ITextPart> EnumerateTextParts(this DirectMessage dm)
+        {
+            return EnumerateTextParts(dm.Text, dm.Entities);
+        }
+    }
+
+    /// <summary>
+    /// Resolved source field
+    /// </summary>
+    public struct Source
+    {
+        /// <summary>
+        /// The name of the client which the user tweeted with.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The URL of the client witch the user tweeted with.
+        /// </summary>
+        public Uri Href { get; set; }
+    }
+
+    /// <summary>
+    /// Types of <see cref="CoreTweet.ITextPart"/>.
+    /// </summary>
+    public enum TextPartType
+    {
+        /// <summary>
+        /// Plain text, which is related to no entity.
+        /// <see cref="CoreTweet.ITextPart.Entity"/> will be <c>null</c>.
+        /// </summary>
+        Plain,
+
+        /// <summary>
+        /// Hashtag.
+        /// <see cref="CoreTweet.ITextPart.Entity"/> will be a <see cref="CoreTweet.HashTag" /> instance.
+        /// </summary>
+        Hashtag,
+
+        /// <summary>
+        /// URL.
+        /// <see cref="CoreTweet.ITextPart.Entity"/> will be a <see cref="CoreTweet.Url" /> instance.
+        /// </summary>
+        Url,
+
+        /// <summary>
+        /// User mention.
+        /// <see cref="CoreTweet.ITextPart.Entity"/> will be a <see cref="CoreTweet.UserMention" /> instance.
+        /// </summary>
+        UserMention
+    }
+
+    /// <summary>
+    /// A part of text.
+    /// </summary>
+    public interface ITextPart
+    {
+        /// <summary>
+        /// The type of this instance.
+        /// </summary>
+        TextPartType Type { get; }
+
+        /// <summary>
+        /// The raw text.
+        /// </summary>
+        string RawText { get; }
+
+        /// <summary>
+        /// The decoded text.
+        /// </summary>
+        string Text { get; }
+
+        /// <summary>
+        /// The base entity information.
+        /// </summary>
+        CoreBase Entity { get; }
+    }
+}
---
+++ CoreTweetSupplementTest/CoreTweetSupplementTest.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreTweet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace CoreTweetSupplementTest
+{
+    [TestClass]
+    public class CoreTweetSupplementTest
+    {
+        private static readonly PrivateType TestTarget = new PrivateType(typeof(CoreTweetSupplement));
+
+        [TestMethod]
+        public void TestCharFromInt()
+        {
+            TestTarget.InvokeStatic("CharFromInt", 97).Is("a");
+            TestTarget.InvokeStatic("CharFromInt", 0x20B9F).Is("𠮟");
+        }
+
+        [TestMethod]
+        public void TestHtmlDecode()
+        {
+            TestTarget.InvokeStatic("HtmlDecode", "test&lt;&gt;a&amp;&quot;&apos;&#9834;&#x266A;0")
+                .Is("test<>a&\"'♪♪0");
+        }
+
+        [TestMethod]
+        public void TestParseSource()
+        {
+            CoreTweetSupplement.ParseSource(@"<a href=""http://twitter.com/download/iphone"" rel=""nofollow"">Twitter for iPhone</a>")
+                .IsStructuralEqual(new Source()
+                {
+                    Name = "Twitter for iPhone",
+                    Href = new Uri("http://twitter.com/download/iphone")
+                });
+            CoreTweetSupplement.ParseSource("web")
+                .IsStructuralEqual(new Source()
+                {
+                    Name = "web",
+                    Href = null
+                });
+        }
+
+        [TestMethod]
+        public void TestEnumerateChar()
+        {
+            ((IEnumerable<string>)TestTarget.InvokeStatic("EnumerateChar", "𠮷野家こそ至高!"))
+                .Is("𠮷", "野", "家", "こ", "そ", "至", "高", "!");
+        }
+
+        [TestMethod]
+        public void TestEnumerateTextParts()
+        {
+            var array = CoreTweetSupplement.EnumerateTextParts("test", null).ToArray();
+            array.Length.Is(1);
+            array[0].Type.Is(TextPartType.Plain);
+            array[0].RawText.Is("test");
+            array[0].Text.Is("test");
+            array[0].Entity.IsNull();
+
+            #region Case 1
+            const string case1 = @"{
+  ""created_at"": ""Wed May 21 09:57:01 +0000 2014"",
+  ""id"": 469054246503989250,
+  ""id_str"": ""469054246503989248"",
+  ""text"": ""てすとtest𠮷野家 #𠮷野家 aa @azyobuzin_test http://t.co/KmtlVpXaUN"",
+  ""source"": ""<a href=\""http://azyobuzi.net/\"" rel=\""nofollow\"">ひゅいっ</a>"",
+  ""truncated"": false,
+  ""in_reply_to_status_id"": null,
+  ""in_reply_to_status_id_str"": null,
+  ""in_reply_to_user_id"": null,
+  ""in_reply_to_user_id_str"": null,
+  ""in_reply_to_screen_name"": null,
+  ""user"": {
+    ""id"": 265093120,
+    ""id_str"": ""265093120"",
+    ""name"": ""ぺぺたろう（テストなーーう）"",
+    ""screen_name"": ""azyobuzin_test"",
+    ""location"": ""@azyobuzinの脳の一部"",
+    ""description"": ""@azyobuzinのテスト用アカウント"",
+    ""url"": ""https://t.co/Fmkl9K35Fu"",
+    ""entities"": {
+      ""url"": {
+        ""urls"": [
+          {
+            ""url"": ""https://t.co/Fmkl9K35Fu"",
+            ""expanded_url"": ""https://twitter.com/azyobuzin"",
+            ""display_url"": ""twitter.com/azyobuzin"",
+            ""indices"": [
+              0,
+              23
+            ]
+          }
+        ]
+      },
+      ""description"": {
+        ""urls"": []
+      }
+    },
+    ""protected"": false,
+    ""followers_count"": 6,
+    ""friends_count"": 2,
+    ""listed_count"": 1,
+    ""created_at"": ""Sun Mar 13 00:52:42 +0000 2011"",
+    ""favourites_count"": 14,
+    ""utc_offset"": 32400,
+    ""time_zone"": ""Tokyo"",
+    ""geo_enabled"": true,
+    ""verified"": false,
+    ""statuses_count"": 84,
+    ""lang"": ""ja"",
+    ""contributors_enabled"": false,
+    ""is_translator"": false,
+    ""is_translation_enabled"": false,
+    ""profile_background_color"": ""C0DEED"",
+    ""profile_background_image_url"": ""http://abs.twimg.com/images/themes/theme1/bg.png"",
+    ""profile_background_image_url_https"": ""https://abs.twimg.com/images/themes/theme1/bg.png"",
+    ""profile_background_tile"": false,
+    ""profile_image_url"": ""http://abs.twimg.com/sticky/default_profile_images/default_profile_5_normal.png"",
+    ""profile_image_url_https"": ""https://abs.twimg.com/sticky/default_profile_images/default_profile_5_normal.png"",
+    ""profile_link_color"": ""0084B4"",
+    ""profile_sidebar_border_color"": ""C0DEED"",
+    ""profile_sidebar_fill_color"": ""DDEEF6"",
+    ""profile_text_color"": ""333333"",
+    ""profile_use_background_image"": true,
+    ""default_profile"": true,
+    ""default_profile_image"": true,
+    ""following"": true,
+    ""follow_request_sent"": false,
+    ""notifications"": false
+  },
+  ""geo"": null,
+  ""coordinates"": null,
+  ""place"": null,
+  ""contributors"": null,
+  ""retweet_count"": 0,
+  ""favorite_count"": 0,
+  ""entities"": {
+    ""hashtags"": [],
+    ""symbols"": [],
+    ""urls"": [],
+    ""user_mentions"": [
+      {
+        ""screen_name"": ""azyobuzin_test"",
+        ""name"": ""ぺぺたろう（テストなーーう）"",
+        ""id"": 265093120,
+        ""id_str"": ""265093120"",
+        ""indices"": [
+          19,
+          34
+        ]
+      }
+    ],
+    ""media"": [
+      {
+        ""id"": 469054246201991200,
+        ""id_str"": ""469054246201991168"",
+        ""indices"": [
+          35,
+          57
+        ],
+        ""media_url"": ""http://pbs.twimg.com/media/BoJqWg1CIAAn2KB.png"",
+        ""media_url_https"": ""https://pbs.twimg.com/media/BoJqWg1CIAAn2KB.png"",
+        ""url"": ""http://t.co/KmtlVpXaUN"",
+        ""display_url"": ""pic.twitter.com/KmtlVpXaUN"",
+        ""expanded_url"": ""http://twitter.com/azyobuzin_test/status/469054246503989248/photo/1"",
+        ""type"": ""photo"",
+        ""sizes"": {
+          ""large"": {
+            ""w"": 616,
+            ""h"": 485,
+            ""resize"": ""fit""
+          },
+          ""thumb"": {
+            ""w"": 150,
+            ""h"": 150,
+            ""resize"": ""crop""
+          },
+          ""small"": {
+            ""w"": 339,
+            ""h"": 267,
+            ""resize"": ""fit""
+          },
+          ""medium"": {
+            ""w"": 599,
+            ""h"": 472,
+            ""resize"": ""fit""
+          }
+        }
+      }
+    ]
+  },
+  ""favorited"": false,
+  ""retweeted"": false,
+  ""possibly_sensitive"": false,
+  ""lang"": ""ja""
+}";
+            #endregion
+
+            array = JsonConvert.DeserializeObject<Status>(case1).EnumerateTextParts().ToArray();
+            array.Length.Is(4);
+            array[0].Type.Is(TextPartType.Plain);
+            array[0].RawText.Is("てすとtest𠮷野家 #𠮷野家 aa ");
+            array[0].Text.Is("てすとtest𠮷野家 #𠮷野家 aa ");
+            array[0].Entity.IsNull();
+            array[1].Type.Is(TextPartType.UserMention);
+            array[1].RawText.Is("@azyobuzin_test");
+            array[1].Text.Is("@azyobuzin_test");
+            array[1].Entity.IsNotNull();
+            array[2].Type.Is(TextPartType.Plain);
+            array[2].RawText.Is(" ");
+            array[2].Text.Is(" ");
+            array[2].Entity.IsNull();
+            array[3].Type.Is(TextPartType.Url);
+            array[3].RawText.Is("http://t.co/KmtlVpXaUN");
+            array[3].Text.Is("pic.twitter.com/KmtlVpXaUN");
+            array[3].Entity.IsNotNull();
+
+            #region Case 2
+            const string case2 = @"{
+  ""created_at"": ""Wed May 21 10:16:15 +0000 2014"",
+  ""id"": 469059084289708000,
+  ""id_str"": ""469059084289708032"",
+  ""text"": ""ってって #test &amp;&lt;てすと&gt;&amp;&amp;amp;"",
+  ""source"": ""<a href=\""http://azyobuzi.net/\"" rel=\""nofollow\"">ひゅいっ</a>"",
+  ""truncated"": false,
+  ""in_reply_to_status_id"": null,
+  ""in_reply_to_status_id_str"": null,
+  ""in_reply_to_user_id"": null,
+  ""in_reply_to_user_id_str"": null,
+  ""in_reply_to_screen_name"": null,
+  ""user"": {
+    ""id"": 265093120,
+    ""id_str"": ""265093120"",
+    ""name"": ""ぺぺたろう（テストなーーう）"",
+    ""screen_name"": ""azyobuzin_test"",
+    ""location"": ""@azyobuzinの脳の一部"",
+    ""description"": ""@azyobuzinのテスト用アカウント"",
+    ""url"": ""https://t.co/Fmkl9K35Fu"",
+    ""entities"": {
+      ""url"": {
+        ""urls"": [
+          {
+            ""url"": ""https://t.co/Fmkl9K35Fu"",
+            ""expanded_url"": ""https://twitter.com/azyobuzin"",
+            ""display_url"": ""twitter.com/azyobuzin"",
+            ""indices"": [
+              0,
+              23
+            ]
+          }
+        ]
+      },
+      ""description"": {
+        ""urls"": []
+      }
+    },
+    ""protected"": false,
+    ""followers_count"": 6,
+    ""friends_count"": 2,
+    ""listed_count"": 1,
+    ""created_at"": ""Sun Mar 13 00:52:42 +0000 2011"",
+    ""favourites_count"": 14,
+    ""utc_offset"": 32400,
+    ""time_zone"": ""Tokyo"",
+    ""geo_enabled"": true,
+    ""verified"": false,
+    ""statuses_count"": 85,
+    ""lang"": ""ja"",
+    ""contributors_enabled"": false,
+    ""is_translator"": false,
+    ""is_translation_enabled"": false,
+    ""profile_background_color"": ""C0DEED"",
+    ""profile_background_image_url"": ""http://abs.twimg.com/images/themes/theme1/bg.png"",
+    ""profile_background_image_url_https"": ""https://abs.twimg.com/images/themes/theme1/bg.png"",
+    ""profile_background_tile"": false,
+    ""profile_image_url"": ""http://abs.twimg.com/sticky/default_profile_images/default_profile_5_normal.png"",
+    ""profile_image_url_https"": ""https://abs.twimg.com/sticky/default_profile_images/default_profile_5_normal.png"",
+    ""profile_link_color"": ""0084B4"",
+    ""profile_sidebar_border_color"": ""C0DEED"",
+    ""profile_sidebar_fill_color"": ""DDEEF6"",
+    ""profile_text_color"": ""333333"",
+    ""profile_use_background_image"": true,
+    ""default_profile"": true,
+    ""default_profile_image"": true,
+    ""following"": true,
+    ""follow_request_sent"": false,
+    ""notifications"": false
+  },
+  ""geo"": null,
+  ""coordinates"": null,
+  ""place"": null,
+  ""contributors"": null,
+  ""retweet_count"": 0,
+  ""favorite_count"": 0,
+  ""entities"": {
+    ""hashtags"": [
+      {
+        ""text"": ""test"",
+        ""indices"": [
+          5,
+          10
+        ]
+      }
+    ],
+    ""symbols"": [],
+    ""urls"": [],
+    ""user_mentions"": []
+  },
+  ""favorited"": false,
+  ""retweeted"": false,
+  ""lang"": ""ja""
+}";
+            #endregion
+
+            array = JsonConvert.DeserializeObject<Status>(case2).EnumerateTextParts().ToArray();
+            array.Length.Is(3);
+            array[0].Type.Is(TextPartType.Plain);
+            array[0].RawText.Is("ってって ");
+            array[0].Text.Is("ってって ");
+            array[0].Entity.IsNull();
+            array[1].Type.Is(TextPartType.Hashtag);
+            array[1].RawText.Is("#test");
+            array[1].Text.Is("#test");
+            array[1].Entity.IsNotNull();
+            array[2].Type.Is(TextPartType.Plain);
+            array[2].RawText.Is(" &amp;&lt;てすと&gt;&amp;&amp;amp;");
+            array[2].Text.Is(" &<てすと>&&amp;");
+            array[2].Entity.IsNull();
+        }
+    }
+}
