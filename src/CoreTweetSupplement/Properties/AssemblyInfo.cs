37ddf339a25be79cda925a70720d39fe9ee5cbe4
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-04-03 01:57:11 +0900

    Migrate to csproj

--- src/CoreTweetSupplement/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("CoreTweetSupplement")]
-[assembly: AssemblyDescription("An utility for client developers with CoreTweet")]
-[assembly: AssemblyCompany("CoreTweet Development Team")]
-[assembly: AssemblyProduct("CoreTweetSupplement")]
